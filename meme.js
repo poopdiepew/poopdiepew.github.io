b1747f3786386cc0e491ac08b2ae67ae8659fd5c
Prince Panja <61006978+poopdiepew@users.noreply.github.com>
2022-02-04 22:50:08 +0530

    Update meme.js

--- meme.js
+++ meme.js
@@ -20,7 +20,7 @@ const search = document.createElement('textArea');
 search.style.overflowWrap = 'true';
 search.style.textOverflow = 'false';
 search.style.height = '30vh';
-search.style.width =  '600px';
+search.style.width =  '75vh';
 search.style.fontSize = '24px';
 search.style.borderRadius = '16px';
 
@@ -38,7 +38,7 @@ submit.addEventListener('click', e=>{
 const output = document.createElement('textArea');
 output.style.textOverflow = 'false';
 output.style.height = '30vh';
-output.style.width =  '600px';
+output.style.width =  '75vh';
 output.style.fontSize = '24px';
 output.readOnly = 'true';
 output.style.borderRadius = '16px';
